8cb3322e4a6e13f9e4277f785b411712d52844a6
LynnT-2003 <u6411271@au.edu>
2024-06-10 15:48:04 +0700

    Fixed .env

--- main.go
+++ main.go
@@ -4,23 +4,32 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
 	"time"
 
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
-
-// Replace with your MongoDB URI
-const mongoURI = "mongodb+srv://lynn:Dabbingdubu208@mv-realty-cluster.rsoihcc.mongodb.net/"
 
 var client *mongo.Client
 
 // Initialize MongoDB client
 func connectMongoDB() {
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	mongoURI := os.Getenv("MONGODB_URI")
+	if mongoURI == "" {
+		log.Fatal("MONGODB_URI environment variable not set")
+	}
+
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	var err error
 	client, err = mongo.Connect(ctx, options.Client().ApplyURI(mongoURI))
 	if err != nil {
 		log.Fatal(err)
