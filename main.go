2300beb3bef0bf647b09af1e5f9b46213ad64c27
LynnT-2003 <u6411271@au.edu>
2024-06-11 09:40:22 +0700

    Completed GET function for Properties.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,11 +12,25 @@ import (
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var client *mongo.Client
+
+type Property struct {
+	ID          primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+	Title       string             `bson:"Title" json:"Title"`
+	Developer   string             `bson:"Developer" json:"Developer"`
+	Description string             `bson:"Description" json:"Description"`
+	Coordinates [2]float64         `bson:"Coordinates" json:"Coordinates"`
+	PriceRange  string             `bson:"Price-range" json:"Price-range"`
+	Facilities  []string           `bson:"Facilities" json:"Facilities"`
+	Built       int                `bson:"Built" json:"Built"`
+	CreatedAt   time.Time          `bson:"Created_at" json:"Created_at"`
+}
 
 // Initialize MongoDB client
 func connectMongoDB() {
@@ -46,6 +61,36 @@ func connectMongoDB() {
 	fmt.Println("Connected to MongoDB!")
 }
 
+func getProperties(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	collection := client.Database("MVDB").Collection("properties")
+	cur, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		http.Error(w, "Failed to retrieve Properties from MongoDB", http.StatusInternalServerError)
+		return
+	}
+	defer cur.Close(ctx)
+
+	var properties []Property
+	for cur.Next(ctx) {
+		var property Property
+		if err := cur.Decode(&property); err != nil {
+			http.Error(w, "Failed to decode retrieved Properties", http.StatusInternalServerError)
+			return
+		}
+		properties = append(properties, property)
+	}
+	if err := cur.Err(); err != nil {
+		http.Error(w, "Error iterating through cursor", http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(properties)
+}
+
 func main() {
 	connectMongoDB()
 	r := mux.NewRouter()
@@ -58,6 +103,9 @@ func main() {
 
 	// Create a new handler with CORS middleware
 	handler := cors(r)
+
+	// Routes
+	r.HandleFunc("/properties", getProperties).Methods("GET")
 
 	port := os.Getenv("PORT")
 	if port == "" {
